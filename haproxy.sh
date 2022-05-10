c07443ef34c34d9db573122479b35e6965a782ab
aunshk <61991375+aunshk@users.noreply.github.com>
2022-05-10 15:33:52 +0530

    Create haproxy.sh

---
+++ haproxy.sh
@@ -0,0 +1,56 @@
+# shell script to setup HAProxy server with roundrobin algorythm in Rocky linux
+#!/bin/bash
+
+# Update system
+sudo yum update -y
+
+# install Haproxy packege
+sudo yum install haproxy -y 
+
+# stop haproxy service
+sudo systemctl stop haproxy
+
+# configure haproxy.conf file
+cd /etc/haproxy
+sudo mv haproxy.cfg haproxy.cfg.backup
+touch haproxy.conf
+
+# add configuration
+sudo bash -c 'cat <<EOT >> /etc/haproxy/haproxy.conf
+global
+   log /dev/log local0
+   log /dev/log local1 notice
+   chroot /var/lib/haproxy
+   stats timeout 30s
+   user haproxy
+   group haproxy
+   daemon
+
+defaults
+   log global
+   mode http
+   option httplog
+   option dontlognull
+   timeout connect 5000
+   timeout client 50000
+   timeout server 50000
+
+frontend http_front
+   bind *:80
+   stats uri /haproxy?stats
+   default_backend http_back
+
+backend http_back
+   balance roundrobin
+   server server_name1 192.168.116.144:8080 check
+   server server_name2 192.168.116.145:80 check
+EOT'
+
+
+# test nginx configuration
+ haproxy -f /etc/haproxy/haproxy.cfg -c
+
+# start haproxy service 
+sudo systemctl start Haproxy
+
+echo "Completed"
